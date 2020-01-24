9b7a4a2427c0d8f2a0b4cc499b48c98af2e6af46
KennyOLaw <kaylenlaw11@gmail.com>
2020-01-24 13:03:06 -0500

    Update user.rb

--- W9D4/tinkerapp/app/models/user.rb
+++ W9D4/tinkerapp/app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true 
   validates :session_token, presence: true, uniqueness: true
   validates :password_digest, presence { message: "Password can't be blank"} 
-  validates :password, length: { minimum: 6, allow_nil: true }
+  validates :password, length: { minimum: 6}, allow_nil: true 
   before_validation :ensure_session_token
   
   attr_reader :password
